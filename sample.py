47002f64294d01b292f10d8897d64d0e40998fab
mausam-office <eng3@ramlaxmangroup.com>
2026-02-06 12:15:53 +0545

    Changes code with error to test

--- sample.py
+++ sample.py
@@ -8,7 +8,7 @@ area = calculate_area(r)
 
 print(f"--- Python Build Script ---")
 print(f"--- This is v.3 ---")
-print(f"--- Auto building SCM Pooling  ---")
+printsd(f"--- Auto building SCM Pooling  ---")
 
-print(f"Radius: {r}")
-print(f"The area of the circle is: {area:.2f}")
+printee(f"Radius: {r}")
+printsd(f"The area of the circle is: {area:.2f}"
