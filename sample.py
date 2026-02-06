94a2f33d97730a6752836684233bab6964009752
mausam-office <eng3@ramlaxmangroup.com>
2026-02-06 11:29:48 +0545

    added'

---
+++ sample.py
@@ -0,0 +1,11 @@
+import math
+
+def calculate_area(radius):
+    return math.pi * (radius ** 2)
+
+r = 5
+area = calculate_area(r)
+
+print(f"--- Python Build Script ---")
+print(f"Radius: {r}")
+print(f"The area of the circle is: {area:.2f}")
