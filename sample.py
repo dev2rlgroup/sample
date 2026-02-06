90ca75c3daa44da07183c0667a57f212783f3f01
mausam-office <eng3@ramlaxmangroup.com>
2026-02-06 12:18:15 +0545

    Bug fixed

--- sample.py
+++ sample.py
@@ -8,7 +8,8 @@ area = calculate_area(r)
 
 print(f"--- Python Build Script ---")
 print(f"--- This is v.3 ---")
-printsd(f"--- Auto building SCM Pooling  ---")
+print(f"--- Auto building SCM Pooling  ---")
 
-printee(f"Radius: {r}")
-printsd(f"The area of the circle is: {area:.2f}"
+print(f"Radius: {r}")
+print(f"The area of the circle is: {area:.2f}")
+print(f"--- Bug fixed  ---")
